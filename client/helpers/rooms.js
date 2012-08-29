0b88df0ccb1122d4afe9e908a09f17b55f9cf417
Jamie Atkinson <jabbslad@gmail.com>
2012-08-29 11:40:51 +0100

    update mention colours

--- client/helpers/rooms.js
+++ client/helpers/rooms.js
@@ -1,3 +1,7 @@
 Handlebars.registerHelper('rooms', function() {
 	return Rooms.find();
 });
+
+Handlebars.registerHelper('is_me', function(mention) {
+	return (mention.id === Meteor.user()._id);
+});
